59322a4537a70310276ba14ae1aed29276484649
Gilar6706160027 <37774151+Gilar6706160027@users.noreply.github.com>
2018-03-26 09:51:45 +0700

    Update Jurnal.java

--- Jurnal.java
+++ Jurnal.java
@@ -18,21 +18,25 @@ public class Jurnal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Fibonacci : ");
-        int n = show.nextInt();
-        int a = 0 ;
-        int b = 1 ;
-        int jumlah=0 ;
-         
-        for(int i = 1; i <=n; i++) {
-            System.out.print(b + " ");
-            b=b+a ;
-            a=b-a ;
-            jumlah = b ;
-        }
-        jumlah = jumlah + b ;
-        System.out.println(jumlah);
+        System.out.print("Masukkan jumlah deret fibonacci: ");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("\n Jumlah fibonacci ke-" + number + " adalah  ==> " + (fibonacciLoop(number) + ""));
     }
-    
+    public static int fibonacciLoop(int number) {
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+
+        int fibo1 = 1, fibo2 = 1, fibonacci = 1, sum=2;
+        for (int i = 3; i <= number; i++) {
+            fibonacci = fibo1 + fibo2; // Fibonacci number is sum of previous two Fibonacci number
+            fibo1 = fibo2;
+            fibo2 = fibonacci; 
+            sum = sum + fibonacci;
+        }
+        System.out.println("jumlah : " + sum);
+        return fibonacci;
+    }
 }
+
+// udah di edit
